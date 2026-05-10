fe0ba845e48a5352edce1e099d83ec74ce85e57e
Azan Asim <148682737+Azan-Asim@users.noreply.github.com>
2026-05-10 21:08:38 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  
   applicationName: "Afia AI",
   title: {
     default: "Afia AI | Investor Overview",
